53b66f7c5aab72ef50a0c1ede181aa66556d3a0b
NauelG <g.nauel@gmail.com>
2018-09-24 22:43:34 +0200

    Creando componentes personalizados de react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function saludar(nombre, how) {
+  alert(`${how} ${nombre}`);
+}
+
+/**
+ * BotonQueSaluda Component
+ * @param {String} name Nombre al que saludar
+ * @param {String} how Tipo de saludo
+ * @return Devuelve un boton que lanza un alert en el click
+ */
+const BotonQueSaluda = ({name, how = "Hola"}) => 
+  <button onClick={() => saludar(name, how)}>Click Me</button>
+
+const Selector = ({items}) => 
+  <select>
+    {items.map(({id, value}) => 
+      <option key={id}>{value}</option>
+    )}
+  </select>
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +33,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      <div>
+        <BotonQueSaluda name="Carlos" how="Hola"/>
+      </div>
+      <div>
+        <Selector items={[{id: 1, value: 'Uno'}, {id: 2, value: 'Dos'}, {id: 3, value: 'Tres'}]}/>
+      </div>
       </div>
     );
   }
